84f2a099c9b9ec78807777d41e48c23c1ee9816c
Eskaan <github@eskaan.de>
2024-12-18 00:30:46 +0100

    Solutions for day 9 (now with working part 2)

--- src/bin/day9-2.rs
+++ src/bin/day9-2.rs
@@ -1,79 +1,70 @@
 fn main() {
     let input = std::fs::read_to_string("input/day9.txt").unwrap();
 
-    let mut layout = Vec::new();
+    // [(id, pos, len)]
+    let mut disk = Vec::new();
     let mut read_space = false;
-    let mut id: u16 = 0;
-
-    for i in input.chars().map(|ch| ch as u8 - b'0') {
-        if read_space {
-            for _ in 0..i {
-                layout.push(None);
-            }
-        } else {
-            for _ in 0..i {
-                layout.push(Some(id));
-            }
-            id = id.checked_add(1).unwrap();
+    let mut index = 0;
+    let mut id = 0;
+    for i in input.trim().chars().map(|ch| (ch as u8 - b'0') as usize) {
+        if !read_space {
+            disk.push((id, index, i));
+            id += 1;
         }
 
+        index += i;
         read_space = !read_space;
     }
 
-    #[rustfmt::skip]
-    println!("{}", layout.iter().map(|opt| opt.map(|id| ((id % 10) as u8 + b'0') as char).unwrap_or('.')).collect::<String>());
+    debug_print(&disk);
 
-    // Use last assigned id
-    let mut count_id = None;
-    let mut layout_count = 0;
-    for layout_index in (0..layout.len()).rev() {
-        if layout[layout_index] == count_id {
-            layout_count += 1;
-        } else {
-            if count_id.is_some() {
-                let mut space_count = 0;
-                for (space_index, id) in layout.iter().enumerate().take(layout_index - layout_count)
-                {
-                    if id.is_some() {
-                        if space_count >= layout_count {
-                            for i in 0..layout_count {
-                                let src = layout_index + 1 + i;
-                                let dst = (space_index + i) - space_count;
-                                println!(
-                                    "Swap {} {:?} - {} {:?}",
-                                    src, layout[src], dst, layout[dst]
-                                );
-                                if src < dst {
-                                    println!("Noop");
-                                    break;
-                                }
-                                assert!(!(layout[src].is_none() || layout[dst].is_some()),);
-                                layout.swap(src, dst);
-                            }
-                            break;
-                        }
-                        space_count = 0;
-                    } else {
-                        space_count += 1;
-                    }
-                }
-                #[rustfmt::skip]
-                println!("{}", layout.iter().map(|opt| opt.map(|id| ((id % 10) as u8 + b'0') as char).unwrap_or('.')).collect::<String>());
-                println!();
+    'id: for id in (1..id).rev() {
+        let file_pos = disk.iter().position(|(fid, _, _)| *fid == id).unwrap();
+        let (id, pos, len) = disk[file_pos];
+        //debug_print(&disk);
+
+        let mut lf_end = 0;
+        for (i, &(fid, fpos, flen)) in disk.iter().enumerate() {
+            if fpos - lf_end >= len {
+                disk.remove(file_pos);
+                disk.insert(i, (id, lf_end, len));
+                continue 'id;
             }
-            count_id = layout[layout_index];
-            layout_count = 1;
+            if fid == id {
+                continue 'id;
+            }
+            lf_end = fpos + flen;
         }
+        disk.remove(file_pos);
+        disk.push((id, pos, len));
     }
 
-    #[rustfmt::skip]
-    println!("{}", layout.iter().map(|opt| opt.map(|id| ((id % 10) as u8 + b'0') as char).unwrap_or('.')).collect::<String>());
+    if !disk.is_sorted_by_key(|(_, pos, _)| pos) {
+        unreachable!("Disk not sorted");
+    }
+
+    debug_print(&disk);
 
     let mut count: usize = 0;
-    for (i, opt_id) in layout.iter().enumerate() {
-        if let Some(id) = opt_id {
-            count = count.checked_add(i * *id as usize).unwrap();
+    for &(id, pos, len) in disk.iter() {
+        for i in pos..pos + len {
+            println!("{i} * {id} = {}", i * id);
+            count = count.checked_add(i * id).unwrap();
         }
     }
     println!("{count}");
+}
+
+fn debug_print(disk: &Vec<(usize, usize, usize)>) {
+    let mut index = 0;
+    for &(id, pos, len) in disk {
+        for _ in index..pos {
+            print!(".");
+        }
+        for _ in 0..len {
+            print!("{}", id % 10);
+        }
+        index = pos + len;
+    }
+    println!();
 }
